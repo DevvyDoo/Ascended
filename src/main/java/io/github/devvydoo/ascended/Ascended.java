a53258bfee18fdac6dce078fe5d143893f6c49f8
Devvy <devinhall4@gmail.com>
2020-01-25 21:13:14 -0500

    oops

--- src/main/java/io/github/devvydoo/ascended/Ascended.java
+++ src/main/java/io/github/devvydoo/ascended/Ascended.java
@@ -5,26 +5,39 @@ import io.github.devvydoo.ascended.exceptions.AscendedErrorHandler;
 import net.dv8tion.jda.api.JDABuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.xml.sax.ErrorHandler;
 import sh.niall.yui.Yui;
 import sh.niall.yui.exceptions.PrefixException;
 import sh.niall.yui.prefix.PrefixManager;
 
 import javax.security.auth.login.LoginException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Ascended {
+
+    private String getDiscordToken(){
+
+        File file = new File("token.txt");
+        try (Scanner scanner = new Scanner(file)){
+            return scanner.nextLine();
+        } catch (FileNotFoundException e){
+            throw new RuntimeException("Could not find valid discord token in token.txt!");
+        }
+    }
 
 
     public static void main(String[] args)  throws PrefixException, LoginException {
 
+        Ascended main = new Ascended();
+
         Logger logger = LoggerFactory.getLogger(Ascended.class);
 
         PrefixManager prefixManager = new PrefixManager("/");
-        JDABuilder jdaBuilder = new JDABuilder("NTgyNzA5MDA5NjkwMTMyNDgy.XOx2wA.fTImAEVlTSYBOj66waXPh3LJufM");
+        JDABuilder jdaBuilder = new JDABuilder(main.getDiscordToken());
 
         Yui yui = new Yui(jdaBuilder, prefixManager, new AscendedErrorHandler());
         yui.addCogs(new Fun());
-
 
         jdaBuilder.build();
     }
